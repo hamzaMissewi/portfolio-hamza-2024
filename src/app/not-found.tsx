6119f997db60d209e12258457bc458b3a143cbe6
Hamza Missaoui <hamza.missaoui47@gmail.com>
2024-10-09 09:28:02 +0100

    new commit for new branch fix clean code

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,9 @@
 export default function NotFound() {
     return (
-        <html>
-        <body className="text-center">
-        <h1 className="mt-10 font-semibold">Something went wrong!</h1>
-        </body>
-        </html>
+        // <body className="text-center">
+        <div>
+            <h1 className="mt-10 font-semibold">Something went wrong!</h1>
+        </div>
+        // </body>
     );
 }

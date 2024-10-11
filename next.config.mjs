29e861997464f08a580e9f9650199ad6ac009dc8
Hamza Missaoui <hamza.missaoui47@gmail.com>
2024-10-11 20:38:18 +0100

    commit 11 10 2024

--- next.config.mjs
+++ next.config.mjs
@@ -4,22 +4,29 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // rewrites: async () => [
-    //     {
-    // source: '/',
-    // source: '/sign-in',
-    // destination: '/api/portfolios', // Adjust to your sign-in path
-    // permanent: false,
-    // },
-    // ],
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/[language]',
+                permanent: true, // 308 permanent redirect
+            },
+        ];
+    },
+    rewrites: async () => [
+        {
+            source: '/api/:path*',
+            destination: 'https://external-api.com/:path*', // Proxy requests
+        },
+    ],
     distDir: 'build',
     reactStrictMode: true,
     pageExtensions: ["html", "jsx", "tsx", "ts"],
     images: {
-        dangerouslyAllowSVG: true,
-        // remotePatterns: [
-        //     {protocol: "https", hostname: "*"},
-        // ],
+        // dangerouslyAllowSVG: true,
+        remotePatterns: [
+            {protocol: "https", hostname: "*"},
+        ],
     },
 };
 

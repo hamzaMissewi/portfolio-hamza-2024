de487b579afa30b7b7bee108c1348ca21cce26f6
Hamza Missaoui <hamza.missaoui47@gmail.com>
2024-10-14 13:58:45 +0100

    use codesandbox portfolio example to test deploy to vercel or netlify

--- next.config.mjs
+++ next.config.mjs
@@ -1,39 +1,4 @@
-import createNextIntlPlugin from "next-intl/plugin";
-
-const withNextIntl = createNextIntlPlugin();
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    async redirects() {
-        return [
-            {
-                source: '/en',
-                destination: '/',
-                permanent: false, // 308 permanent redirect
-            },
-            {
-                source: '/home',
-                destination: '/',
-                permanent: false, // 308 permanent redirect
-            },
-        ];
-    },
-    // rewrites: async () => [
-    //     {
-    //         source: '/api/:path*',
-    //         destination: 'https://external-api.com/:path*', // Proxy requests
-    //     },
-    // ],
-    distDir: 'build',
-    reactStrictMode: true,
-    pageExtensions: ["html", "jsx", "tsx", "ts"],
-    images: {
-        dangerouslyAllowSVG: true,
-        remotePatterns: [
-            {protocol: "https", hostname: "*"},
-        ],
-    },
-};
+const nextConfig = {};
 
-export default withNextIntl(nextConfig);
-// export default nextConfig;
+export default nextConfig;

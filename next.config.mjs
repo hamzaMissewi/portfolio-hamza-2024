95027af6e2a586659514575fdb792de8c55e589e
Hamza Missaoui <hamza.missaoui47@gmail.com>
2024-10-15 16:36:20 +0100

    🔥🚀☑️

--- next.config.mjs
+++ next.config.mjs
@@ -9,19 +9,18 @@ const nextConfig = {
       // { hostname: "*", pathname: "*" },
       {
         // Pattern for images from a specific domain
-        protocol: "https",
-        hostname: "*",
-        port: "", // Specify the port if needed, or leave it empty
         pathname: "./public/**", // Use ** for wildcard matching
+        // protocol: "https",
+        // hostname: "*",
       },
-// {
-      //   protocol: "https",
-      //   hostname: "res.cloudinary.com",
-      // },
-      // {
-      //   protocol: "https",
-      //   hostname: "s3-eu-west-1.amazonaws.com",
-      // },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "s3-eu-west-1.amazonaws.com",
+      },
     ],
   },
 };

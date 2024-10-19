b1cd38a82cf621baf6a61850bb4100d1d115db7d
Hamza Missaoui <hamza.missaoui47@gmail.com>
2024-10-19 17:17:08 +0100

    🚀☑️ fix footer

--- app/(admin)/studio/[...index]/page.tsx
+++
@@ -1,7 +0,0 @@
-// import { NextStudio } from "next-sanity/studio";
-// import { config } from "@/sanity";
-//
-// export default function StudioPage() {
-//   return sanity;
-//   // return <NextStudio config={config} />;
-// }

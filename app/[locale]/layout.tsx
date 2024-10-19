d2030d24b8407861c52fe619fa86c46405bf10a7
Hamza Missaoui <hamza.missaoui47@gmail.com>
2024-10-19 10:51:26 +0100

    🔥🚀☑️ add sanity and it works for test for experience

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,58 @@
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import { Inter } from "next/font/google";
+import { ThemeProvider } from "@/theme/provider";
+import type { Metadata } from "next";
+import { siteConfig } from "@/config";
+import React, { PropsWithChildren } from "react";
+// import localFont from "next/dist/compiled/@next/font/dist/local";
+
+// const arFont = localFont({
+//   src: "@/fonts/Rubik-MediumItalic.ttf",
+// });
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = siteConfig;
+
+// export const viewport: Viewport = {
+//   themeColor: "#000319",
+//   colorScheme: "dark",
+// };
+
+interface RootLayoutProps extends PropsWithChildren {
+  children: React.ReactNode;
+  params: { locale: string };
+}
+
+async function Layout({ children, params: { locale } }: RootLayoutProps) {
+  const messages = await getMessages();
+  // const clerkFrontendApi = process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+  return (
+    <html lang={locale} dir={locale === "ar" ? "rtl" : "ltr"}>
+      <head>
+        <title>Hamza Missaoui Portfolio</title>
+        <link rel="icon" href="/hamza/b2b-alive.svg" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body
+        // className={locale === "ar" ? arFont.className : inter.className}
+        className={inter.className}
+        suppressHydrationWarning={true}
+      >
+        <NextIntlClientProvider messages={messages}>
+          <ThemeProvider
+            themes={["dark", "light"]}
+            attribute="class"
+            defaultTheme="dark"
+          >
+            {children}
+          </ThemeProvider>
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
+
+export default Layout;

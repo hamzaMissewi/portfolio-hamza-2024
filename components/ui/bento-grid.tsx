a6eae5c9954fc5b6aefdf428adf4dd085579e78c
Hamza Missaoui <hamza.missaoui47@gmail.com>
2024-10-19 18:53:59 +0100

    🚀☑️ fix

--- components/ui/bento-grid.tsx
+++ components/ui/bento-grid.tsx
@@ -34,163 +34,162 @@ export const BentoGrid = ({
   );
 };
 
-export const BentoGridIte,
-  m = ({
-    id,
-    className,
-    title,
-    description,
-    img,
-    imgClassName,
-    titleClassName,
-    spareImg,
-  }: {
-    id?: number;
-    className?: string;
-    // title?: string | React.ReactNode;
-    title?: string;
-    description?: string | React.ReactNode;
-    img?: string;
-    imgClassName?: string;
-    titleClassName?: string;
-    spareImg?: string;
-  }) => {
-    const [copied, setCopied] = useState(false);
+export const BentoGridItem = ({
+  id,
+  className,
+  title,
+  description,
+  img,
+  imgClassName,
+  titleClassName,
+  spareImg,
+}: {
+  id?: number;
+  className?: string;
+  // title?: string | React.ReactNode;
+  title?: string;
+  description?: string | React.ReactNode;
+  img?: string;
+  imgClassName?: string;
+  titleClassName?: string;
+  spareImg?: string;
+}) => {
+  const [copied, setCopied] = useState(false);
 
-    const handleCopy = () => {
-      navigator.clipboard.writeText(links.ownerEmail);
-      setCopied(true);
-    };
+  const handleCopy = () => {
+    navigator.clipboard.writeText(links.ownerEmail);
+    setCopied(true);
+  };
 
-    useEffect(() => {
-      if (!copied) return;
+  useEffect(() => {
+    if (!copied) return;
 
-      const copyTimeout = setTimeout(() => {
-        setCopied(false);
-      }, 3500);
+    const copyTimeout = setTimeout(() => {
+      setCopied(false);
+    }, 3500);
 
-      return () => clearTimeout(copyTimeout);
-    }, [copied]);
+    return () => clearTimeout(copyTimeout);
+  }, [copied]);
 
-    return (
-      <div
-        className={cn(
-          "group/bento relative row-span-1 flex flex-col justify-between space-y-4 overflow-hidden rounded-3xl border border-white/[0.1] shadow-input transition duration-200 hover:shadow-xl dark:shadow-none",
-          className
-        )}
-        style={{
-          background: "rgb(4,7,29)",
-          backgroundColor:
-            "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
-        }}
-      >
-        <div className={cn("h-full", id === 6 && "flex justify-center")}>
-          <div className="absolute h-full w-full">
-            {img && (
-              <Image
-                width={689}
-                height={541}
-                src={img}
-                alt={img}
-                className={cn("object-cover object-center", imgClassName)}
-              />
-            )}
+  return (
+    <div
+      className={cn(
+        "group/bento relative row-span-1 flex flex-col justify-between space-y-4 overflow-hidden rounded-3xl border border-white/[0.1] shadow-input transition duration-200 hover:shadow-xl dark:shadow-none",
+        className
+      )}
+      style={{
+        background: "rgb(4,7,29)",
+        backgroundColor:
+          "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+      }}
+    >
+      <div className={cn("h-full", id === 6 && "flex justify-center")}>
+        <div className="absolute h-full w-full">
+          {img && (
+            <Image
+              width={689}
+              height={541}
+              src={img}
+              alt={img}
+              className={cn("object-cover object-center", imgClassName)}
+            />
+          )}
+        </div>
+
+        <div
+          className={cn(
+            // "absolute right-0 -mb-5",
+            "relative",
+            id === 5 && "w-full opacity-80"
+          )}
+        >
+          {spareImg && (
+            <Image
+              width={208}
+              height={96}
+              src={spareImg}
+              alt={spareImg}
+              className="h-full w-full object-cover object-center"
+            />
+          )}
+        </div>
+
+        {id === 6 && <BackgroundGradientAnimation />}
+
+        <div
+          className={cn(
+            "relative flex min-h-40 flex-col p-5 px-5 transition duration-200 group-hover/bento:translate-x-2 md:h-full lg:p-10",
+            titleClassName
+          )}
+        >
+          <div className="z-10 font-sans text-sm font-extralight text-[#c1c2d3] md:text-xs lg:text-base">
+            {description}
           </div>
 
-          <div
-            className={cn(
-              // "absolute right-0 -mb-5",
-              "relative",
-              id === 5 && "w-full opacity-80"
-            )}
-          >
-            {spareImg && (
-              <Image
-                width={208}
-                height={96}
-                src={spareImg}
-                alt={spareImg}
-                className="h-full w-full object-cover object-center"
-              />
-            )}
+          <div className="z-10 max-w-96 font-sans text-lg font-bold lg:text-3xl">
+            {title}
           </div>
 
-          {id === 6 && <BackgroundGradientAnimation />}
+          {id === 2 && <GridGlobe />}
 
-          <div
-            className={cn(
-              "relative flex min-h-40 flex-col p-5 px-5 transition duration-200 group-hover/bento:translate-x-2 md:h-full lg:p-10",
-              titleClassName
-            )}
-          >
-            <div className="z-10 font-sans text-sm font-extralight text-[#c1c2d3] md:text-xs lg:text-base">
-              {description}
-            </div>
+          {id === 3 && (
+            <div className="relative -right-3 flex w-fit gap-1 lg:-right-2 lg:gap-5">
+              <div className="flex flex-col gap-3 lg:gap-8">
+                {techStack.stack1.map((item) => (
+                  <span
+                    key={item}
+                    className="rounded-lg bg-[#10132e] px-3 py-2 text-center text-xs opacity-50 lg:px-3 lg:py-4 lg:text-base lg:opacity-100"
+                  >
+                    {item}
+                  </span>
+                ))}
 
-            <div className="z-10 max-w-96 font-sans text-lg font-bold lg:text-3xl">
-              {title}
-            </div>
-
-            {id === 2 && <GridGlobe />}
-
-            {id === 3 && (
-              <div className="relative -right-3 flex w-fit gap-1 lg:-right-2 lg:gap-5">
-                <div className="flex flex-col gap-3 lg:gap-8">
-                  {techStack.stack1.map((item) => (
-                    <span
-                      key={item}
-                      className="rounded-lg bg-[#10132e] px-3 py-2 text-center text-xs opacity-50 lg:px-3 lg:py-4 lg:text-base lg:opacity-100"
-                    >
-                      {item}
-                    </span>
-                  ))}
-
-                  <span className="rounded-lg bg-[#10132e] px-3 py-4 text-center" />
-                </div>
-
-                <div className="flex flex-col gap-3 lg:gap-8">
-                  <span className="rounded-lg bg-[#10132e] px-3 py-4 text-center" />
-                  {techStack.stack2.map((item) => (
-                    <span
-                      key={item}
-                      className="rounded-lg bg-[#10132e] px-3 py-2 text-center text-xs opacity-50 lg:px-3 lg:py-4 lg:text-base lg:opacity-100"
-                    >
-                      {item}
-                    </span>
-                  ))}
-                </div>
+                <span className="rounded-lg bg-[#10132e] px-3 py-4 text-center" />
               </div>
-            )}
 
-            {id === 6 && (
-              <div className="group relative mt-5">
-                <button
-                  tabIndex={-1}
-                  className="pointer-events-none absolute -bottom-5 right-0 cursor-default"
-                >
-                  <Lottie
-                    options={{
-                      loop: copied,
-                      autoplay: copied,
-                      animationData,
-                      rendererSettings: {
-                        preserveAspectRatio: "xMidYMid slice",
-                      },
-                    }}
-                  />
-                </button>
+              <div className="flex flex-col gap-3 lg:gap-8">
+                <span className="rounded-lg bg-[#10132e] px-3 py-4 text-center" />
+                {techStack.stack2.map((item) => (
+                  <span
+                    key={item}
+                    className="rounded-lg bg-[#10132e] px-3 py-2 text-center text-xs opacity-50 lg:px-3 lg:py-4 lg:text-base lg:opacity-100"
+                  >
+                    {item}
+                  </span>
+                ))}
+              </div>
+            </div>
+          )}
 
-                <MagicButton
-                  title={copied ? "Email copied!" : "Copy my email"}
-                  icon={<IoCopyOutline />}
-                  otherClasses="!bg-[#161a31]"
-                  handleClick={handleCopy}
-                  asChild
+          {id === 6 && (
+            <div className="group relative mt-5">
+              <button
+                tabIndex={-1}
+                className="pointer-events-none absolute -bottom-5 right-0 cursor-default"
+              >
+                <Lottie
+                  options={{
+                    loop: copied,
+                    autoplay: copied,
+                    animationData,
+                    rendererSettings: {
+                      preserveAspectRatio: "xMidYMid slice",
+                    },
+                  }}
                 />
-              </div>
-            )}
-          </div>
+              </button>
+
+              <MagicButton
+                title={copied ? "Email copied!" : "Copy my email"}
+                icon={<IoCopyOutline />}
+                otherClasses="!bg-[#161a31]"
+                handleClick={handleCopy}
+                asChild
+              />
+            </div>
+          )}
         </div>
       </div>
-    );
-  };
+    </div>
+  );
+};
